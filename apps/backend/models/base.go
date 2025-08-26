bfec5d694b38d035dc8655425e1c4161c4885d6f
companydream <tbrantleyii@gmail.com>
2025-08-26 12:36:39 -0400

    update libraries

--- apps/backend/models/base.go
+++ apps/backend/models/base.go
@@ -28,3 +28,31 @@ type UserInput struct {
 	// Department is optional.
 	Department *string `json:"department" form:"department"`
 }
+
+type UserDTO struct {
+	ID         string  `json:"id"`
+	UserName   string  `json:"user_name"`
+	FirstName  string  `json:"first_name"`
+	LastName   string  `json:"last_name"`
+	Email      string  `json:"email"`
+	Department *string `json:"department"`
+}
+
+// Paging defines the structure for pagination information
+// @Description Paging
+type Paging struct {
+	// The current page number
+	Page *int `json:"page"`
+	// The total number of items available
+	PageCount *int `json:"page_count"`
+	// The number of items per page
+	PageSize *int `json:"page_size"`
+}
+
+// UserDTOPaging defines the structure for user pagination which includes the users and pagination details
+// @Description UserDTOPaging
+type UserPaging struct {
+	Paging // Embedded Paging struct
+	// A list of users
+	Users []UserDTO `json:"users"`
+}
