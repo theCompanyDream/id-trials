23e379107b732fbd4902499c8d6af535aa91258d
companydream <tbrantleyii@gmail.com>
2026-01-17 09:52:13 -0500

    vite is fun

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
@@ -10,5 +11,15 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist/frontend'
+  },
+  resolve: {
+    alias: {
+      '@backpack': path.resolve(__dirname, './src/components/index'),
+      '@backpack/*': path.resolve(__dirname, './src/components/*'),
+      // If you need the wildcards to work for assets/pages too:
+      '@components': path.resolve(__dirname, './src/components'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+    }
   }
 })
