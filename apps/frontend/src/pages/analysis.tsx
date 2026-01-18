d1513e73adcb90e1f4e78316d7f5720b34ccf8d2
theCompanyDream <tbrantleyii@gmail.com>
2026-01-18 17:09:47 -0500

    add analytics

--- apps/frontend/src/pages/analysis.tsx
+++ apps/frontend/src/pages/analysis.tsx
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Analysis = () => {
+	const [tableSize, setTableSize] = useState()
 
 	useEffect(() => {
+		fetch(`/analytics/tableSize?`)
+			.then(data => data.json())
+			.then(table => setTableSize(table))
 
 	}, []);
 
